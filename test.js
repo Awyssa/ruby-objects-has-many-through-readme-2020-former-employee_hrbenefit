2bfcda6809f2583273f83eeb3ad99e155ab7bacd
Michael Ernest <m.hendersun@gmail.com>
2021-01-28 01:46:43 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,13 @@
+const sweetNewCar = {
+    color: "red",
+    model: "Stingray",
+    make: "Corvette",
+    year: 2018
+}
+
+
+function car(array) {
+  console.log(sweetNewCar.year);
+}
+
+car(sweetNewCar)
